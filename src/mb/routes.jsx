4d7d0e495020a43e57458de18ff150bbdcc1d6a3
lilongllong <2453085960@qq.com>
2017-03-01 18:59:40 +0800

    Add search route in routers.js

--- src/mb/routes.jsx
+++ src/mb/routes.jsx
@@ -4,11 +4,13 @@ import { IndexRoute, Route } from 'react-router';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import MoviePage from './containers/MoviePage';
+import SearchPage from './containers/SearchPage';
 
 export function configRoutes() {
   return (
     <Route path="/" component={App}>
       <IndexRoute component={HomePage} />
+      <Route path="/search" component={SearchPage} />
       <Route path="/:movieId" component={MoviePage} />
     </Route>
   );
