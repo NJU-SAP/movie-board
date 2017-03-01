ce97103a917a3bc0df2c9447ac8fe810a2f2241f
lilongllong <2453085960@qq.com>
2017-03-01 18:58:14 +0800

    Add SearchPage.jsx in containers.

---
+++ src/mb/containers/SearchPage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import actionCreators from '../actions/search-action-creators';
+import SearchPage from '../components/SearchPage';
+
+@connect(
+  (state, ownProps) => ({
+    searchResult: state.search.searchResult,
+    searchValue: ownProps.location.query.value
+  }),
+  dispatch => ({ actions: bindActionCreators(actionCreators, dispatch) })
+)
+export default class MoviePage extends React.Component {
+  static propTypes = {
+    searchResult: React.PropTypes.array,
+    searchValue: React.PropTypes.string.isRequired,
+    actions: React.PropTypes.func.isRequired,
+  };
+
+  static defaultProps = {
+    searchResult: null,
+    searchValue: '',
+    actions: null,
+  }
+
+  render() {
+    return (
+      <SearchPage
+        searchResult={this.props.searchResult}
+        searchValue={this.props.searchValue}
+        actions={this.props.actions}
+      />
+    );
+  }
+}
