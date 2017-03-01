1ef8ca17fbfceb217755c97e162d2c84415b9b52
lilongllong <2453085960@qq.com>
2017-03-01 18:59:11 +0800

    Add searchPage in components.

---
+++ src/mb/components/SearchPage.jsx
@@ -0,0 +1,36 @@
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
+
+export default class Search extends Component {
+  static propTypes = {
+    searchValue: PropTypes.string.isRequired,
+    searchResult: PropTypes.array,
+    actions: PropTypes.shape({
+      search: PropTypes.func.isRequired,
+      setSearchValue: PropTypes.func.isRequired,
+    })
+  };
+
+  static default = {
+    searchValue: null,
+    searchResult: null,
+    actions: null,
+  };
+
+  state = {
+    data: this.props.searchResult
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.searchValue !== this.props.searchValue) {
+      this.props.actions.search(nextProps.searchValue);
+    }
+  }
+
+  render() {
+    if (this.props.searchResult) {
+      return (<div className="mb-search-page">{this.props.searchResult.toString()}</div>);
+    }
+    return null;
+  }
+}
