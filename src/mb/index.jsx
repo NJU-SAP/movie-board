ce042319c180c2aac28b303df428549813f550ba
LucasYuNju <LucasYuNju@gmail.com>
2017-03-05 13:53:35 +0800

    Update package.json

--- src/mb/index.jsx
+++ src/mb/index.jsx
@@ -5,8 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import Root from './Root';
 import configStore from './store';
 
-const initialState = __app_initial_state__;
-const store = configStore();
+const initialState = window.__app_initial_state__;
+const store = configStore(initialState);
 
 render(
   <BrowserRouter>
