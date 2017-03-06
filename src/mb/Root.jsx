316e9563c6696624c58b92787f51044de3ebeccc
LucasYuNju <LucasYuNju@gmail.com>
2017-03-06 10:08:13 +0800

    Lazy load topicPage

--- src/mb/Root.jsx
+++ src/mb/Root.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router';
 
+import asyncComponent from './components/asyncComopnent';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import MoviePage from './containers/MoviePage';
 import SearchPage from './containers/SearchPage';
-import TopicPage from './containers/TopicPage';
+
+const TopicPage = asyncComponent(
+  () => import('./containers/TopicPage').then(module => module.default)
+);
 
 export default function Root(props) {
   return (
@@ -15,8 +19,8 @@ export default function Root(props) {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/search" component={SearchPage} />
-          <Route path="/topic/:topic" component={TopicPage} />
           <Route path="/movie/:movieId" component={MoviePage} />
+          <Route path="/topic/:topic" component={TopicPage} />
         </Switch>
       </App>
     </Provider>
